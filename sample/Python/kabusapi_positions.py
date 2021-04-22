88e29dd4255fbc27f48787ba31e02908125d6c99
Xiaolong Lu <lu@kabu.com>
2021-04-22 13:48:35 +0900

    updated Python sample code files

--- sample/Python/kabusapi_positions.py
+++ sample/Python/kabusapi_positions.py
@@ -3,8 +3,10 @@ import json
 import pprint
 
 url = 'http://localhost:18080/kabusapi/positions'
-params = { 'product': 0 }   # product - 0:すべて、1:現物、2:信用、3:先物、4:OP
-#params['symbol'] = '9433'  # symbol='xxxx'
+params = { 'product': 0 }   	# product - 0:すべて、1:現物、2:信用、3:先物、4:OP
+params['symbol'] = '9433'  		# symbol='xxxx'
+params['side'] = '1' 				# 1:売、2:買
+params['addinfo'] = 'false' 	# true:追加情報を出力する、false:追加情報を出力しない　※追加情報は、「現在値」、「評価金額」、「評価損益額」、「評価損益率」を意味します
 req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)), method='GET')
 req.add_header('Content-Type', 'application/json')
 req.add_header('X-API-KEY', '43e7cc3611fd476db35e93c36a3f77ef')
